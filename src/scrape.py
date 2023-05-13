a220bff60b2d4476d210983b2168b907408a2ad2
jainr3 <Rahul96Jain@gmail.com>
2023-05-13 11:53:33 -0400

    fixed call to account for spaces

--- src/scrape.py
+++ src/scrape.py
@@ -3,13 +3,16 @@ import requests
 import urllib, urllib.request
 
 def arxiv_search(search_term, start_idx=0, max_results=200):
+    search_term = search_term.replace(" ", "%20")
     BASE_URL = "http://export.arxiv.org/api/query?search_query="
     
     data = urllib.request.urlopen(f"{BASE_URL}" + search_term + f"&start={start_idx}" + f"&max_results={max_results}")
     
     return data.read().decode('utf-8')
 
+def googlescholar_search(search_term):
+    pass
 
 
 if __name__ == "__main__":
-    print(arxiv_search("deeplearning", max_results=5))
+    print(arxiv_search("deep learning", max_results=5))
