cf1ba5854ad3091997d8c8a15b7f97cd9175e262
mcguirecooper <50084830+mcguirecooper@users.noreply.github.com>
2023-05-13 15:46:25 -0400

    let him cook

--- src/rank.py
+++ src/rank.py
@@ -13,44 +13,52 @@ import io
 import anthropic
 import PyPDF2
 
+
 # Set up your cohere client
 API_KEY = "ROZ1XaiS9AgwHygUhXRK6xhDbxQXroQ9xURzqxFE"
 co = cohere.Client(API_KEY)
 
 client = anthropic.Client('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')
 
-# Example query and passages (data taken from http://sbert.net/datasets/simplewiki-2020-11-01.jsonl.gz)
 print("What can we help you find?")
-query = str(input())
-arxis_docs = parse.arxiv_parsing(query)
+first_query = str(input())
 
-results = co.rerank(query=query, documents=arxis_docs, top_n=3, model='rerank-english-v2.0') # Change top_n to change the number of results returned. If top_n is not passed, all results will be returned.
-best_article_index = 100
+# Example query and passages (data taken from http://sbert.net/datasets/simplewiki-2020-11-01.jsonl.gz)
+summary_prompt = f"{anthropic.HUMAN_PROMPT} You are an expert in the {first_query}. I want to conduct \
+  an inclusive search in an Academic database for papers to figure out “What you want to learn”. Provide me \
+    with the best search criteria to use with the database to find papers relevant to my topic. The suggested \
+      search should try to be as inclusive as possible to result in the most papers possible on the topic. I \
+        want the only response to this message to be what I should put in the search bar.  {anthropic.AI_PROMPT}"
+
+response = client.completion(
+    prompt= summary_prompt,
+    stop_sequences = [anthropic.HUMAN_PROMPT],
+    model="claude-v1-100k",
+    max_tokens_to_sample=5000,
+)
+
+second_query = response['completion']
+
+arxis_docs = parse.arxiv_parsing(second_query)
+
+results = co.rerank(query=second_query, documents=arxis_docs, top_n=5, model='rerank-english-v2.0') # Change top_n to change the number of results returned. If top_n is not passed, all results will be returned.
+
 for idx, r in enumerate(results):
   print(f"Document Rank: {idx + 1}, ArXiv Relevancy Index: {r.index}")
   print(f"Document: {r.document['text']}")
-  print(f"Relevance Score: {r.relevance_score:.2f}")
+  print('Title: ' + str(arxis_docs[list(arxis_docs)[idx]]["title"]))
+  # print(f"Relevance Score: {r.relevance_score:.2f}")
   print("\n")
-  if r.index < best_article_index:
-    best_article_index = r.index
 
-url = arxis_docs[list(arxis_docs)[best_article_index]]["pdf_url"]
+print('Which document would you like summarized?')
+which_summary = int(input())
 
-r = requests.get(url)
-f = io.BytesIO(r.content)
-
-reader = PyPDF2.PdfReader(f)
-
-contents = ""
-
-for i in range(len(reader.pages)):
-    page_contents = reader.pages[i].extract_text()
-    contents += page_contents + " "
+url = arxis_docs[list(arxis_docs)[which_summary]]["pdf_url"]
+contents = parse.arxiv_pdf_parsing(url)
 
 summary_prompt = f"{anthropic.HUMAN_PROMPT} Teach me how this {contents} works by asking questions about my level of understanding \
-  of necessary concepts. With each response, fill in gaps in my understanding, then recursively ask me more \
-  questions to check my understanding. Identify and share the most important 20% of learnings from this paper \
-  that will help me understand 80% of it. {anthropic.AI_PROMPT}"
+  of necessary concepts. Identify and share the most important 20% of learnings from this paper \
+  that will help me understand 80% of it. After summarizing the article, do not add any other text. {anthropic.AI_PROMPT}"
 
 response = client.completion(
     prompt= summary_prompt,
@@ -59,3 +67,4 @@ response = client.completion(
     max_tokens_to_sample=5000,
 )
 print(response['completion'])
+print('Source: ' + str(url))
