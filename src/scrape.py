2d026e79ead4b71b8e30e7af1b717321ba39621e
jainr3 <Rahul96Jain@gmail.com>
2023-05-13 13:03:09 -0400

    update scrape

--- src/scrape.py
+++ src/scrape.py
@@ -1,6 +1,6 @@
 """collect webpage result from query"""
-import requests
 import urllib, urllib.request
+from bs4 import SoupStrainer, BeautifulSoup
 
 def arxiv_search(search_term, start_idx=0, max_results=200, sort_by="relevance"):
     search_term = search_term.replace(" ", "%20")
@@ -12,9 +12,16 @@ def arxiv_search(search_term, start_idx=0, max_results=200, sort_by="relevance")
     
     return data.read().decode('utf-8')
 
-def googlescholar_search(search_term):
-    pass
+# https://stackoverflow.com/questions/13200709/extract-google-scholar-results-using-python-or-r
+class GoogleScholarOpener(urllib.request.FancyURLopener):
+    version = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
 
+def googlescholar_search(search_term):
+    openurl = GoogleScholarOpener().open
+    unparsed = openurl(f"http://scholar.google.se/scholar?hl=en&q={search_term}").read()
+    page = BeautifulSoup(unparsed, parse_only=SoupStrainer('div', id='gs_ab_md'), features="lxml")
+    return page
 
 if __name__ == "__main__":
     print(arxiv_search("deep learning", max_results=5, sort_by="lastUpdatedDate"))
+    print(googlescholar_search("deep learning"))
