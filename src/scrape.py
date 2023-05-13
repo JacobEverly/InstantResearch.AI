12998b96b5b408731848600f5008085145609ee3
jainr3 <Rahul96Jain@gmail.com>
2023-05-13 12:02:06 -0400

    update arxiv scrape

--- src/scrape.py
+++ src/scrape.py
@@ -2,11 +2,13 @@
 import requests
 import urllib, urllib.request
 
-def arxiv_search(search_term, start_idx=0, max_results=200):
+def arxiv_search(search_term, start_idx=0, max_results=200, sort_by="relevance"):
     search_term = search_term.replace(" ", "%20")
     BASE_URL = "http://export.arxiv.org/api/query?search_query="
     
-    data = urllib.request.urlopen(f"{BASE_URL}" + search_term + f"&start={start_idx}" + f"&max_results={max_results}")
+    assert(sort_by in ["relevance", "lastUpdatedDate", "submittedDate"])
+
+    data = urllib.request.urlopen(f"{BASE_URL}" + search_term + f"&start={start_idx}" + f"&max_results={max_results}" + f"&sortBy={sort_by}")
     
     return data.read().decode('utf-8')
 
@@ -15,4 +17,4 @@ def googlescholar_search(search_term):
 
 
 if __name__ == "__main__":
-    print(arxiv_search("deep learning", max_results=5))
+    print(arxiv_search("deep learning", max_results=5, sort_by="lastUpdatedDate"))
